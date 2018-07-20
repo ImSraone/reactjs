6af3b97a46d2164df4455b2a0a1c612e4e65c186
Sravan <sravan.vanteru@gmail.com>
2018-07-20 13:17:17 +0530

    basic setup

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class AboutPage extends React.Component{
+    render(){
+        return(
+            
+            <div>
+                <p>About Page </p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
--- tools/startMessage.js
+++ tools/startMessage.js
@@ -1,6 +1,7 @@
 import colors from 'colors';
 
-/* eslint-disable no-console */
+/* eslint-disable no-console */cls
+
 
 console.log('Starting app in dev mode...'.green);
 
